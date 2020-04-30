9761bc215c123242273c1fe6db30cd64efcdf8c8
Cornflakes691 <madimichael@outlook.com>
2020-04-30 14:11:30 -0700

    organization

---
+++ Firmware/sound reaction/sound_lights.ino
@@ -0,0 +1,39 @@
+const int mic = 0; //pin 0
+const int led = 5; //pin 5
+int pwm = 0;
+int micRaw = 0; //raw
+int valc = 0;
+int samplec = 0; //sample counter
+float  LEDout = 0; //processed value for led
+
+void setup()
+
+{
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+
+}
+
+void loop()
+{
+
+  while (samplec < 100) {
+
+    micRaw = analogRead(mic);
+    micRaw = (micRaw - 522);
+    valc = valc + micRaw;
+    samplec++;
+    Serial.println(valc);
+    delay(100);
+  }
+
+LEDout = valc/100;
+
+//Serial.println("value");
+Serial.println(LEDout);
+
+//Serial.println("RAW");
+//Serial.println(micRaw);
+
+  //Serial.print(out);
+}
