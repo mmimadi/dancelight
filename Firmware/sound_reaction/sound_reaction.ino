f0bd90b098a7e6dff8c165d0f117698305f68eae
Cornflakes691 <madimichael@outlook.com>
2020-05-01 14:54:58 -0700

    updated  expenses

--- Firmware/sound_reaction/sound_reaction.ino
+++ Firmware/sound_reaction/sound_reaction.ino
@@ -1,45 +1,39 @@
 const int mic = 0; //pin 0
 const int led = 5; //pin 5
 int pwm = 0;
-float micRaw = 0; //raw
-float valc = 0;
+int micRaw = 0; //raw
+int valc = 0;
 int samplec = 0; //sample counter
 float  LEDout = 0; //processed value for led
-int percent = 0;
 
 void setup()
-
+If
 {
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-micRaw = analogRead(mic);
+
 }
 
 void loop()
 {
 
-  while (samplec < 100) {
+  while (samplec < 1000) {
 
-    
-   // micRaw = (micRaw - 522);
-    valc = valc + micRaw;
+    micRaw = analogRead(mic);
+    //micRaw = (micRaw - 522);
+   // valc = valc + micRaw;
     samplec++;
     Serial.println(micRaw);
     delay(100);
   }
 
-//LEDout = (valc / 100);
-//percent = (LEDout * 1) + percent;
+LEDout = valc/100;
 
+//Serial.println("value");
+Serial.println(LEDout);
 
-Serial.println(percent);
-micRaw = analogRead(mic);
-if (micRaw > 4) {
-  analogWrite(led, 255);
-  delay(15);
+//Serial.println("RAW");
+//Serial.println(micRaw);
 
-} else {
-  analogWrite(led, 20);
-}
-
+  //Serial.print(out);
 }
