c07ae045b73b3d0bcd88b860c5cc94ca688002fe
Cornflakes691 <madimichael@outlook.com>
2020-05-02 02:29:37 -0700

    fade for sound light

--- Firmware/sound_reaction/sound_reaction.ino
+++ Firmware/sound_reaction/sound_reaction.ino
@@ -1,12 +1,11 @@
 ///////SEXY REVISION//////
 
-
 int micInputPin = 0;
 int ledPin = 5;
 int sampleCounter = 0;
 
-float micRawValue = 10;
-float percent = 200; //set sound threshold percentage
+float micRawValue = 0;
+float percent = 100; //set sound threshold percentage
 float processedResult = 0;
 
 
@@ -20,7 +19,7 @@ void loop() {
 
   while (sampleCounter < 100) {
 
-    //micRawValue = analogRead(micInputPin); //uncomment with actual mic attached
+    micRawValue = analogRead(micInputPin); //uncomment with actual mic attached
     processedResult = processedResult + micRawValue;
     delay(7);
     Serial.println(micRawValue); //debug
@@ -35,20 +34,42 @@ void loop() {
   processedResult = ((processedResult * percent) + processedResult) / sampleCounter;
 
   ///////////////debug///////////////////
-  Serial.println("DB threshold"); 
+  Serial.println("DB threshold");
   Serial.println(processedResult);
   //////////////////////////////////////
 
-  //micRawValue = analogRead(micInputPin); //uncomment with actual mic attached
+  micRawValue = analogRead(micInputPin); //uncomment with actual mic attached
   pinMode(ledPin, OUTPUT);
 
   if (micRawValue > processedResult) {
 
-    analogWrite(ledPin, 255); //use a DAC to generate sine wave and create a fade?
-    delay(15);
+    SoundFade();
 
   } else {
 
     analogWrite(ledPin, 20); //standbye brightness
   }
+}
+
+void SoundFade() {
+
+  analogWrite(ledPin, 255);
+  delay(30);
+  analogWrite(ledPin, 210);
+  delay(10);
+  analogWrite(ledPin, 200);
+  delay(25);
+  micRawValue = analogRead(micInputPin);
+  analogWrite(ledPin, 190);
+  delay(25);
+  analogWrite(ledPin, 150);
+  delay(25);
+  micRawValue = analogRead(micInputPin);
+  analogWrite(ledPin, 110);
+  delay(25);
+  analogWrite(ledPin, 50);
+  delay(25);
+  analogWrite(ledPin, 5);
+  delay(100);
+  micRawValue = analogRead(micInputPin);
 }
