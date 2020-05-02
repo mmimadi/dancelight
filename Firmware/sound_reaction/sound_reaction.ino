36e646e4d5e5a65c37fe3130e6e4708601864b06
Cornflakes691 <madimichael@outlook.com>
2020-05-02 01:16:22 -0700

    sexy revision for sound reaction

--- Firmware/sound_reaction/sound_reaction.ino
+++ Firmware/sound_reaction/sound_reaction.ino
@@ -1,39 +1,54 @@
-const int mic = 0; //pin 0
-const int led = 5; //pin 5
-int pwm = 0;
-int micRaw = 0; //raw
-int valc = 0;
-int samplec = 0; //sample counter
-float  LEDout = 0; //processed value for led
+///////SEXY REVISION//////
 
-void setup()
-If
-{
+
+int micInputPin = 0;
+int ledPin = 5;
+int sampleCounter = 0;
+
+float micRawValue = 10;
+float percent = 200; //set sound threshold percentage
+float processedResult = 0;
+
+
+void setup() {
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
-
+  percent = percent / 100;
 }
 
-void loop()
-{
 
-  while (samplec < 1000) {
+void loop() {
 
-    micRaw = analogRead(mic);
-    //micRaw = (micRaw - 522);
-   // valc = valc + micRaw;
-    samplec++;
-    Serial.println(micRaw);
-    delay(100);
+  while (sampleCounter < 100) {
+
+    //micRawValue = analogRead(micInputPin); //uncomment with actual mic attached
+    processedResult = processedResult + micRawValue;
+    delay(7);
+    Serial.println(micRawValue); //debug
+    sampleCounter++;
   }
+  ////////////////debug///////////////////
+  Serial.println("totaled");
+  Serial.println(processedResult);
+  delay(100);
+  ///////////////////////////////////////
 
-LEDout = valc/100;
+  processedResult = ((processedResult * percent) + processedResult) / sampleCounter;
 
-//Serial.println("value");
-Serial.println(LEDout);
+  ///////////////debug///////////////////
+  Serial.println("DB threshold"); 
+  Serial.println(processedResult);
+  //////////////////////////////////////
 
-//Serial.println("RAW");
-//Serial.println(micRaw);
+  //micRawValue = analogRead(micInputPin); //uncomment with actual mic attached
+  pinMode(ledPin, OUTPUT);
 
-  //Serial.print(out);
+  if (micRawValue > processedResult) {
+
+    analogWrite(ledPin, 255); //use a DAC to generate sine wave and create a fade?
+    delay(15);
+
+  } else {
+
+    analogWrite(ledPin, 20); //standbye brightness
+  }
 }
