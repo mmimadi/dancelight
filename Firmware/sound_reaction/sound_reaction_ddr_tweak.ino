aaa0109340018baaccb1abc187beae6ea984720b
DDR <d@ddr0.ca>
2020-05-08 16:18:44 -0700

    Tweaked sound_reaction to work as a loop. Untested.
    	- I don't have a mic here.

---
+++ Firmware/sound_reaction/sound_reaction_ddr_tweak.ino
@@ -0,0 +1,92 @@
+///////SEXY REVISION//////
+
+unsigned int micInputPin = 0;
+unsigned int ledPin = 5;
+float sampleCounter = 0;
+float micRawValue = 0;
+float processedResult = 0;
+
+float percent = 10; //Set the % above the ambient sound to hit the light
+int samples = 10; //Set # samples to calculate ambient sound
+unsigned int howBumpingIsIt = 0;
+const unsigned int ITS_TOTALLY_LIT = 27;
+
+void setup() {
+  Serial.begin(9600);
+  percent = percent / 100;
+}
+
+void loop() {
+
+  while (sampleCounter <= samples) {
+
+    micRawValue = analogRead(micInputPin); //uncomment with actual mic attached
+    processedResult = processedResult + micRawValue;
+
+    delay(100);
+    Serial.println(micRawValue); //debug
+    sampleCounter++;
+
+    while (sampleCounter == samples + 1) {
+      processedResult = ((processedResult * percent) + processedResult) / sampleCounter;
+      sampleCounter++;
+    }
+
+  }
+  
+  analogWrite(ledPin, 5); //standby brightness
+
+  // Serial.println(processedResult);
+  BumpIt(); //LETTSS GOOOOOOOO
+  if (howBumpingIsIt) { SoundFade(); }
+}
+
+void BumpIt() {
+
+  pinMode(ledPin, OUTPUT);
+   micRawValue = analogRead(micInputPin);
+  Serial.println(micRawValue);
+
+  if (micRawValue > processedResult) {
+    howBumpingIsIt = ITS_TOTALLY_LIT;
+  }
+}
+
+void writeAndDelay(unsigned int brightness, unsigned int ms) {
+  analogWrite(ledPin, brightness);
+  delay(ms);
+}
+
+void SoundFade() {
+  switch (howBumpingIsIt) {
+    case 27: writeAndDelay(255, 60); break;
+    case 26: writeAndDelay(250, 20); break;
+    case 25: writeAndDelay(240, 20); break;
+    case 24: writeAndDelay(230, 20); break;
+    case 23: writeAndDelay(220, 20); break;
+    case 22: writeAndDelay(210, 30); break;
+    case 21: writeAndDelay(200, 30); break;
+    case 20: writeAndDelay(190, 30); break;
+    case 19: writeAndDelay(180, 30); break;
+    case 18: writeAndDelay(170, 20); break;
+    case 17: writeAndDelay(160, 30); break;
+    case 16: writeAndDelay(150, 40); break;
+    case 15: writeAndDelay(140, 50); break;
+    case 14: writeAndDelay(130, 55); break;
+    case 13: writeAndDelay(120, 60); break;
+    case 12: writeAndDelay(110, 50); break;
+    case 11: writeAndDelay(100, 50); break;
+    case 10: writeAndDelay( 90, 50); break;
+    case  9: writeAndDelay( 80, 50); break;
+    case  8: writeAndDelay( 70, 30); break;
+    case  7: writeAndDelay( 60, 30); break;
+    case  6: writeAndDelay( 50, 30); break;
+    case  5: writeAndDelay( 40, 30); break;
+    case  4: writeAndDelay( 30, 30); break;
+    case  3: writeAndDelay( 20, 30); break;
+    case  2: writeAndDelay( 10, 30); break;
+    case  1: writeAndDelay(  5, 10); break;
+  }
+
+  howBumpingIsIt--;
+}
