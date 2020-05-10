55d5c37ba454e37408de2bbdf4185ef4b53745cf
Cornflakes691 <madimichael@outlook.com>
2020-05-09 17:07:38 -0700

    stuff

--- Firmware/sound_reaction/sound_reaction.ino
+++ Firmware/sound_reaction/sound_reaction.ino
@@ -6,8 +6,8 @@ float sampleCounter = 0;
 float micRawValue = 0;
 float processedResult = 0;
 
-float percent = 10; //Set the % above the ambient sound to hit the light
-int samples = 10; //Set # samples to calculate ambient sound
+float percent = 3.3   ; //Set the % above the ambient sound to hit the light
+int samples = 100; //Set # samples to calculate ambient sound
 
 void setup() {
   Serial.begin(9600);
@@ -18,22 +18,41 @@ void loop() {
 
   while (sampleCounter <= samples) {
 
-    micRawValue = analogRead(micInputPin); //uncomment with actual mic attached
+    Serial.println("processedResult");
+    Serial.println(processedResult);
+    delay(10);
+
+    micRawValue = analogRead(micInputPin);
     processedResult = processedResult + micRawValue;
 
-    delay(100);
+    delay(0);
     Serial.println(micRawValue); //debug
     sampleCounter++;
 
     while (sampleCounter == samples + 1) {
-      processedResult = ((processedResult * percent) + processedResult) / sampleCounter;
+      //   processedResult = ((processedResult * percent) + processedResult) / sampleCounter; //old method
+      processedResult = (processedResult / sampleCounter);
+
+      //Ready animation
+      analogWrite(ledPin, 255);
+      delay(20);
+      analogWrite(ledPin, 10);
+      delay(20);
+      analogWrite(ledPin, 255);
+      delay(20);
+      analogWrite(ledPin, 10);
+
       sampleCounter++;
+
+
     }
 
   }
 
- // Serial.println(processedResult);
-micRawValue = analogRead(micInputPin);
+
+  // Serial.println(processedResult);
+  micRawValue = analogRead(micInputPin);
+  delay(0);
   BumpIt(); //LETTSS GOOOOOOOO
 
 }
@@ -41,14 +60,14 @@ micRawValue = analogRead(micInputPin);
 void BumpIt() {
 
   pinMode(ledPin, OUTPUT);
-   micRawValue = analogRead(micInputPin);
+  micRawValue = analogRead(micInputPin);
   Serial.println(micRawValue);
 
-  if (micRawValue > processedResult) {
+  if (micRawValue > (processedResult * percent) + processedResult || micRawValue < (processedResult * percent) - processedResult) {
     SoundFade();
 
-  } else {
 
+  } else {
     analogWrite(ledPin, 5); //standbye brightness
   }
 }
@@ -56,97 +75,100 @@ void BumpIt() {
 void SoundFade() {
 
   analogWrite(ledPin, 255);
-  BumpIt();
-  delay(60);
-  BumpIt();
-  analogWrite(ledPin, 250);
-  delay(20);
-  BumpIt();
-  analogWrite(ledPin, 240);
-  delay(20);
-  BumpIt();
-  analogWrite(ledPin, 230);
-  delay(20);
-  BumpIt();
-  analogWrite(ledPin, 220);
-  delay(20);
-  BumpIt();
-  analogWrite(ledPin, 210);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 200);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 190);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 180);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 170);
-  delay(20);
-  BumpIt();
-  analogWrite(ledPin, 160);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 150);
-  delay(40);
-  BumpIt();
-  analogWrite(ledPin, 140);
-  delay(50);
-  BumpIt();
-  analogWrite(ledPin, 130);
-  delay(55);
-  BumpIt();
-  analogWrite(ledPin, 120);
-  delay(60);
-  BumpIt();
-  analogWrite(ledPin, 110);
-  delay(50);
-  BumpIt();
-  analogWrite(ledPin, 100);
-  delay(50);
-  BumpIt();
-  analogWrite(ledPin, 90);
-  delay(50);
-  BumpIt();
-  analogWrite(ledPin, 80);
-  delay(50);
-  BumpIt();
-  analogWrite(ledPin, 70);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 60);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 50);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 40);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 30);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 20);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 10);
-  delay(30);
-  BumpIt();
-  analogWrite(ledPin, 5);
-  delay(10);
-  BumpIt();
- // analogWrite(ledPin, 200);
- // delay(50);
- // BumpIt();
- // analogWrite(ledPin, 10);
- // delay(50);
-//  BumpIt();
-//  analogWrite(ledPin, 100);
- // delay(30);
- // BumpIt();
- // analogWrite(ledPin, 5);
- // delay(50);
- // BumpIt();
+  Serial.println("sound fade");
+  delay(0);
+
+  /* BumpIt();
+    analogWrite(ledPin, 250);
+    delay(20);
+    BumpIt();
+    analogWrite(ledPin, 240);
+    delay(20);
+    BumpIt();
+    analogWrite(ledPin, 230);
+    delay(20);
+    BumpIt();
+    analogWrite(ledPin, 220);
+    delay(20);
+    BumpIt();
+    analogWrite(ledPin, 210);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 200);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 190);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 180);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 170);
+    delay(20);
+    BumpIt();
+    analogWrite(ledPin, 160);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 150);
+    delay(40);
+    BumpIt();
+    analogWrite(ledPin, 140);
+    delay(50);
+    BumpIt();
+    analogWrite(ledPin, 130);
+    delay(55);
+    BumpIt();
+    analogWrite(ledPin, 120);
+    delay(60);
+    BumpIt();
+    analogWrite(ledPin, 110);
+    delay(50);
+    BumpIt();
+    analogWrite(ledPin, 100);
+    delay(50);
+    BumpIt();
+    analogWrite(ledPin, 90);
+    delay(50);
+    BumpIt();
+    analogWrite(ledPin, 80);
+    delay(50);
+    BumpIt();
+    analogWrite(ledPin, 70);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 60);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 50);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 40);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 30);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 20);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 10);
+    delay(30);
+    BumpIt();
+    analogWrite(ledPin, 5);
+    delay(10);
+    BumpIt();
+    // analogWrite(ledPin, 200);
+    // delay(50);
+    // BumpIt();
+    // analogWrite(ledPin, 10);
+    // delay(50);
+    //  BumpIt();
+    //  analogWrite(ledPin, 100);
+    // delay(30);
+    // BumpIt();
+    // analogWrite(ledPin, 5);
+    // delay(50);
+    // BumpIt();
+
+  */
 }
