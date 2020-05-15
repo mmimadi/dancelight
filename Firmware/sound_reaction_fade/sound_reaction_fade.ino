423589ab940f0075209ab7ff378b0d0328d03da1
DDR <d@ddr0.ca>
2020-05-15 03:20:11 -0700

    Made sound_reaction_fade have an instant mode, like sound_reaction.

--- Firmware/sound_reaction_fade/sound_reaction_fade.ino
+++ Firmware/sound_reaction_fade/sound_reaction_fade.ino
@@ -10,8 +10,13 @@ float percent = 1; //Set the % above the ambient sound to hit the light
 int samples = 20; //Set # samples to calculate ambient sound
 int grabSample = 0;
 int grabCounter = 0;
+
+const int MODE_INSTANT = 0;
+const int MODE_FADE = 1;
+const int MODE = MODE_INSTANT;
+
 unsigned int howBumpingIsIt = 0;
-const unsigned int ITS_TOTALLY_LIT = 27;
+const unsigned int ITS_TOTALLY_LIT = MODE == MODE_INSTANT ? 2 : 27;
 
 void setup() {
   Serial.begin(9600);
@@ -98,34 +103,41 @@ void writeAndDelay(unsigned int brightness, unsigned int ms) {
 }
 
 void SoundFade() {
-  switch (howBumpingIsIt) {
-    case 27: writeAndDelay(255, 60); break;
-    case 26: writeAndDelay(250, 20); break;
-    case 25: writeAndDelay(240, 20); break;
-    case 24: writeAndDelay(230, 20); break;
-    case 23: writeAndDelay(220, 20); break;
-    case 22: writeAndDelay(210, 30); break;
-    case 21: writeAndDelay(200, 30); break;
-    case 20: writeAndDelay(190, 30); break;
-    case 19: writeAndDelay(180, 30); break;
-    case 18: writeAndDelay(170, 20); break;
-    case 17: writeAndDelay(160, 30); break;
-    case 16: writeAndDelay(150, 40); break;
-    case 15: writeAndDelay(140, 50); break;
-    case 14: writeAndDelay(130, 55); break;
-    case 13: writeAndDelay(120, 60); break;
-    case 12: writeAndDelay(110, 50); break;
-    case 11: writeAndDelay(100, 50); break;
-    case 10: writeAndDelay( 90, 50); break;
-    case  9: writeAndDelay( 80, 50); break;
-    case  8: writeAndDelay( 70, 30); break;
-    case  7: writeAndDelay( 60, 30); break;
-    case  6: writeAndDelay( 50, 30); break;
-    case  5: writeAndDelay( 40, 30); break;
-    case  4: writeAndDelay( 30, 30); break;
-    case  3: writeAndDelay( 20, 30); break;
-    case  2: writeAndDelay( 10, 30); break;
-    case  1: writeAndDelay(  5, 10); break;
+  if (MODE == MODE_INSTANT) {
+    switch (howBumpingIsIt) {
+      case  2: writeAndDelay(255,  0); break;
+      case  1: writeAndDelay(  0,  0); break;
+    }
+  } else {
+    switch (howBumpingIsIt) {
+      case 27: writeAndDelay(255, 60); break;
+      case 26: writeAndDelay(250, 20); break;
+      case 25: writeAndDelay(240, 20); break;
+      case 24: writeAndDelay(230, 20); break;
+      case 23: writeAndDelay(220, 20); break;
+      case 22: writeAndDelay(210, 30); break;
+      case 21: writeAndDelay(200, 30); break;
+      case 20: writeAndDelay(190, 30); break;
+      case 19: writeAndDelay(180, 30); break;
+      case 18: writeAndDelay(170, 20); break;
+      case 17: writeAndDelay(160, 30); break;
+      case 16: writeAndDelay(150, 40); break;
+      case 15: writeAndDelay(140, 50); break;
+      case 14: writeAndDelay(130, 55); break;
+      case 13: writeAndDelay(120, 60); break;
+      case 12: writeAndDelay(110, 50); break;
+      case 11: writeAndDelay(100, 50); break;
+      case 10: writeAndDelay( 90, 50); break;
+      case  9: writeAndDelay( 80, 50); break;
+      case  8: writeAndDelay( 70, 30); break;
+      case  7: writeAndDelay( 60, 30); break;
+      case  6: writeAndDelay( 50, 30); break;
+      case  5: writeAndDelay( 40, 30); break;
+      case  4: writeAndDelay( 30, 30); break;
+      case  3: writeAndDelay( 20, 30); break;
+      case  2: writeAndDelay( 10, 30); break;
+      case  1: writeAndDelay(  5, 10); break;
+    }
   }
   howBumpingIsIt--;
 }
