73e25b6e2cd54d6f6b65eda462304ac1726bcf74
mmimadi <mmimadi@yahoo.com>
2020-05-29 14:19:47 -0700

    pre amp rtest code update

--- Firmware/pre_amp_test/pre_amp_test.ino
+++ Firmware/pre_amp_test/pre_amp_test.ino
@@ -1,14 +1,17 @@
 int mic = 0;
 int val = 0;
+int led = 5;
 
 void setup() {
 
   Serial.begin(9600);
   pinMode(mic, INPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   val = analogRead(mic);
+  analogWrite(led, 50);
 
   Serial.println(val);
   
