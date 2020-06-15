7847f0e528964b8814ec587fd9836736bd2a55de
Cornflakes691 <madimichael@outlook.com>
2020-06-14 20:52:24 -0700

    Added JTAG programming interface and decoupling cap on amp supply

--- Firmware/pre_amp_test/pre_amp_test.ino
+++ Firmware/pre_amp_test/pre_amp_test.ino
@@ -1,19 +1,19 @@
 int mic = 0;
 int val = 0;
-int led = 5;
+
 
 void setup() {
 
   Serial.begin(9600);
   pinMode(mic, INPUT);
-  pinMode(led, OUTPUT);
+ 
 }
 
 void loop() {
   val = analogRead(mic);
-  analogWrite(led, 100);
+  
 
   Serial.println(val);
   
-  //delay() ;
+
 }
