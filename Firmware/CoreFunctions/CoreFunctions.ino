249b72e19aff1f76e6c535affd484219c3800187
Cornflakes691 <madimichael@outlook.com>
2020-06-17 01:41:58 -0700

    Completed button handling code with inturupts

--- Firmware/CoreFunctions/CoreFunctions.ino
+++ Firmware/CoreFunctions/CoreFunctions.ino
@@ -2,8 +2,8 @@
 
 const byte button = 2;
 
-byte buttonstate = 0;
-
+byte buttonState = 0; //HIGH/LOW
+byte buttonHandler = 0; //Variable Count
 byte counter = 0;
 
 byte doubleTapSleep = 0;
@@ -12,67 +12,71 @@ byte doubleTapState = 0;
 byte led = 5;
 byte DevicePower = 0;
 
-
 void setup() {
 
   Serial.begin(9600);
+  pinMode(button, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(button), iHandler, FALLING);
 
   pinMode(led, OUTPUT);
   pinMode(button, INPUT);
-  buttonstate = digitalRead(button);
+  buttonState = digitalRead(button);
 
 }
 
 void loop() {
-  buttonstate = digitalRead(button);
 
+  buttonState = 0;
   doubleTapSleep = 0;
   doubleTapState = 0;
   counter = 0;
 
   Serial.println("MAIN");
 
-  while (buttonstate == LOW && counter < 250) {
+  while (buttonHandler >= 1 && counter < 250) {   //!!!should be ==, >= is for crappy (temporary) debounce!!!
     doubleTapState = 1;
-    buttonstate = digitalRead(button);
+    buttonState = digitalRead(button);
 
+    if (buttonState == HIGH) { //Exit on button release
+      buttonHandler = 0;
+    }
     if (counter > 248 && DevicePower == 0) {
-      //Serial.print("on");
       counter = 0;
       PowerUp();
     }
-
     if (counter > 248 && DevicePower == 1) {
-      //Serial.print(doubleTapSleep);
       counter = 0;
       PowerDown();
     }
-
     counter++;
-    Serial.println(counter); /////!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    delay(7);
   }
 
   while (doubleTapState == 1) {
-    buttonstate = digitalRead(button);
-    doubleTapSleep++;
-    Serial.println(doubleTapSleep); ///////////////////!!!!!!!!!!!!!!!!!!!!!!!!
 
-    if (buttonstate == LOW) {
+    buttonState = digitalRead(button);
+    if (buttonState == HIGH) {
+      buttonHandler = 0;
+    }
+    doubleTapSleep++;
+    delay(5);
+
+    if (buttonState == LOW) {
       doubleTapState = 2;
       BatteryReport();
-
     }
-
-    if (doubleTapSleep > 30) {
+    if (doubleTapSleep > 30) { //sleep timeout
       doubleTapState = 0;
     }
-
   }
+
 }
 
-
-
-
+void iHandler() {
+  sei();
+  buttonHandler++;
+  cli();
+}
 
 //Power Sequences
 int BatteryReport() {
@@ -90,6 +94,7 @@ int BatteryReport() {
 }
 
 int PowerUp() {
+
   analogWrite(led, 10);
   delay(70);
   analogWrite(led, 50);
@@ -107,6 +112,7 @@ int PowerUp() {
 }
 
 int PowerDown() {
+
   analogWrite(led, 255);
   delay(70);
   analogWrite(led, 200);
@@ -120,7 +126,7 @@ int PowerDown() {
   analogWrite(led, 10);
   delay(1000);
   analogWrite(led, 0);
-  
+
   DevicePower = 0;
   //psu pullup
   //low power mode
