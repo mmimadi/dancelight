63bff538f8f790146729da0a5784c0b62754e125
Cornflakes691 <madimichael@outlook.com>
2020-06-23 03:00:20 -0700

    improved boot and shut down animations, fixed eeprom bug, check battery by double tapping even if off, psu fault checking for production, added tap to wake and now defaults to ON while we work on stuff. Note, modes are temporarily disabled

--- Firmware/combined/combined.ino
+++ Firmware/combined/combined.ino
@@ -3,58 +3,44 @@
 #include<avr/sleep.h>
 #include<avr/wdt.h>
 
-
-
 #define END 0
 #define START 1
 #define BLINK 2
 #define UBLINK 3
 #define REPEAT 4
-
 #define LOG_SEEK false
 #define LOG_EXEC false
 #define LOG_MODE false
 
 static const byte micInputPin = 0;
-static const byte ledPin = 5; 
-const byte button = 2; 
-
+static const byte ledPin = 5;
+const byte button = 2;
 byte buttonState = 0; //HIGH/LOW
 byte buttonHandler = 0; //Double Tap Variable Count
 byte holdCounter = 0;
 byte doubleTapSleep = 0;
 byte doubleTapState = 0;
 byte powerStatus = 1;
-static byte mode = 2;
-
-
-//
+static byte mode = 1;
 
 void setup() {
   Serial.begin(57600);
   pinMode(ledPin, OUTPUT);
   resetPreset();
-//  powerDown();
-
   pinMode(button, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(button), iHandler, FALLING);
   pinMode(button, INPUT);
-
-  //mode = EEPROM.read(0);
 }
 
 void loop() {
-  //Monitor preset advancement button and run right preset.
-//  static const byte numModes = 3;
-//  static const unsigned long buttonPressConfirmationTimeMs = 50;
+  buttonLogic();
+  modeHandler();
+  Serial.println(mode);
+}
 
-  //static bool pressed = true;
- // static bool lastProcessedPressed = false;
-//  static unsigned long eventTimeMs = 0;
+///////New Interupt Based Button////////////
 
- 
-
-  /////////////New Intertupt Based Button////////////////////
+void buttonLogic() {
 
   buttonState = 0;
   doubleTapSleep = 0;
@@ -63,16 +49,8 @@ void loop() {
 
   while (buttonHandler >= 1 && holdCounter < 250) {   //!!!should be ==, >= is for crappy (temporary) debounce!!!
     doubleTapState = 1;
-
     buttonState = digitalRead(button);
 
-
-    if(powerStatus == 0){
-      analogWrite(ledPin, 255);
-      delay(1000);
-      analogWrite(ledPin, 0);
-      Sleep();
-    }
     if (buttonState == HIGH) { //Exit on button release
       buttonHandler = 0;
       mode++;
@@ -90,14 +68,13 @@ void loop() {
   }
 
   while (doubleTapState == 1) {
-
+    analogWrite(ledPin, 200);
     buttonState = digitalRead(button);
     if (buttonState == HIGH) {
       buttonHandler = 0;
     }
     doubleTapSleep++;
     delay(5);
-
     if (buttonState == LOW) {
       doubleTapState = 2;
       BatteryReport();
@@ -105,11 +82,25 @@ void loop() {
     if (doubleTapSleep > 30) { //sleep timeout
       doubleTapState = 0;
     }
+    if (powerStatus == 0 && doubleTapSleep > 30) {
+      delay(2000);
+      doubleTapState = 0;
+      Sleep();
+    }
   }
 
 
-  ////////////Old Button Code///////////////////
+  ////////////Old Polling Button Code///////////////////
   /*
+    Monitor preset advancement button and run right preset.
+    static const byte numModes = 3;
+    static const unsigned long buttonPressConfirmationTimeMs = 50;
+
+    static bool pressed = true;
+    static bool lastProcessedPressed = false;
+    static unsigned long eventTimeMs = 0;
+
+
     int buttonState = digitalRead(buttonPin);
     if (buttonState == LOW) { // low means the D2 pin is pulledPin to ground aka connect D2 to GND on the arduino
      if (!pressed) {
@@ -129,28 +120,34 @@ void loop() {
     }
 
   */
-  Serial.println(mode);
+
+}
+
+void modeHandler() {
+
   if (mode == 1) {
     LOG_MODE && Serial.println("mode 1: react to sound");
     //ringBufferLoop(); //use ring buffer beat detection
 
-     LOG_MODE && Serial.println("mode 3: solid");
+    LOG_MODE && Serial.println("mode 3: solid");
     analogWrite(ledPin, 255);
-    
+
   } else if (mode == 2) {
     LOG_MODE && Serial.println("mode 2: blink");
     //processPreset();
-    
-   LOG_MODE && Serial.println("mode 3: solid");
+
+    LOG_MODE && Serial.println("mode 3: solid");
     analogWrite(ledPin, 255);
-    
+
   } else if (mode == 3) {
     LOG_MODE && Serial.println("mode 3: solid");
     analogWrite(ledPin, 255);
+
   } else if (mode >= 4) {
     Serial.print("Bad mode: ");
     Serial.print(mode);
     Serial.println(". Reset to 1.");
     mode = 1;
   }
+
 }
--- Firmware/combined/power_sequences.ino
+++ Firmware/combined/power_sequences.ino
@@ -1,10 +1,11 @@
 //Power Sequences
 
 void iHandler() {
-  //sei();
- 
+  sei();
+  sleep_disable();
   buttonHandler++;
-  // cli();
+
+  cli();
 }
 
 int BatteryReport() {
@@ -26,66 +27,73 @@ int BatteryReport() {
   analogWrite(ledPin, 255);
   delay(100);
   analogWrite(ledPin, 0);
-  delay(2000);
+  delay(1000);
 
+  if (powerStatus == 0) {
+    Sleep();
+  }
 
 }
 
 int PowerUp() {
   sleep_disable();
   mode--;
-  analogWrite(ledPin, 10);
-  delay(70);
-  analogWrite(ledPin, 50);
-  delay(70);
-  analogWrite(ledPin, 100);
-  delay(70);
-  analogWrite(ledPin, 150);
-  delay(70);
-  analogWrite(ledPin, 200);
-  delay(70);
-  analogWrite(ledPin, 255);
-  delay(1000);
-  powerStatus = 1;
+
+  int intensity = 0;
+  while (intensity < 254) {
+    intensity ++;
+    //intensity ++;
+    analogWrite(ledPin, intensity);
+    delay(11);
+  }
   mode = EEPROM.read(0);
-  void setup();
+  powerStatus = 1;
+  delay(500);
+
   //PSU pullup
 }
 
 int PowerDown() {
+
   mode--;
-  analogWrite(ledPin, 255);
-  delay(70);
-  analogWrite(ledPin, 200);
-  delay(70);
-  analogWrite(ledPin, 150);
-  delay(70);
-  analogWrite(ledPin, 100);
-  delay(70);
-  analogWrite(ledPin, 50);
-  delay(70);
-  analogWrite(ledPin, 10);
-  EEPROM.write(0, mode);
-  delay(1000);
-  analogWrite(ledPin, 0);
+  int intensity = 255;
+  while (intensity > 1) {
+    intensity --;
+    //  intensity --;
+    analogWrite(ledPin, intensity);
+    delay(4);
+  }
+   analogWrite(ledPin, 0);
+   EEPROM.write(0, mode);
   powerStatus = 0;
+  delay(500);
   Sleep();
+
+  /*Debug for Production! If the LED stays on, the power supply is faulty and won't turn off
+    analogWrite(ledPin, 200);
+    delay(500);
+    PSU Pull Down
+  */
+
 }
 
-int Sleep() {
+//////sleep needs optimization////////////
 
-  ADCSRA = 0;
-  MCUSR = 0;
-  WDTCSR = bit (WDCE) | bit (WDE);
-  WDTCSR = bit (WDIE) | bit (WDP3) | bit (WDP0);    // set WDIE, and 8 seconds delay
-  wdt_reset();
+int Sleep() {
+  analogWrite(ledPin, 0);
+  // ADCSRA = 0;
+  //  MCUSR = 0;
+  //WDTCSR = bit (WDCE) | bit (WDE);
+  //WDTCSR = bit (WDIE) | bit (WDP3) | bit (WDP0);    // set WDIE, and 8 seconds delay
+  // wdt_reset();
   set_sleep_mode (SLEEP_MODE_PWR_DOWN);
-  noInterrupts ();           
+  //noInterrupts ();
   sleep_enable();
-  MCUCR = bit (BODS) | bit (BODSE);
-  MCUCR = bit (BODS);
-  interrupts ();             
+  //  MCUCR = bit (BODS) | bit (BODSE);
+  // MCUCR = bit (BODS);
+  interrupts ();
   sleep_cpu ();
+  sleep_disable();
 
 
 }
