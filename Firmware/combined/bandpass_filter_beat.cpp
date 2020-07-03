29497d98804e72325b310dcf53d7148d07e0c0a3
DDR <d@ddr0.ca>
2020-07-02 17:43:43 -0700

    Made bandpass mode fade like goertzel beat detection mode.

--- Firmware/combined/bandpass_filter_beat.cpp
+++ Firmware/combined/bandpass_filter_beat.cpp
@@ -10,7 +10,9 @@
 #include "bandpass_filter_beat.hpp"
 
 // Our Global Sample Rate, 5000hz
-#define SAMPLEPERIODUS 200
+// #define SAMPLEPERIODUS 200
+
+static const int BandpassFilterBeat::MODE = BandpassFilterBeat::MODE_FADE;
 
 BandpassFilterBeat::BandpassFilterBeat() {
     // Set ADC to 77khz, max for 10bit
@@ -57,37 +59,90 @@ const float BandpassFilterBeat::beatFilter(float sample) {
 }
 
 void BandpassFilterBeat::loop() {
-    unsigned long time = micros(); // Used to track rate
+    static unsigned long nextSampleTime = 0; // Used to track rate. A bit optimistic, quite frankly, with skew wandering between 0, 8, 255, and a handful of other values.
     static uint8_t sample_count = 0;
-    static unsigned long nextSampleTime = 0;
 
     unsigned long currentSampleTime = micros();
-    if (currentSampleTime < nextSampleTime) { return; }
-    nextSampleTime = currentSampleTime + 20;
-    sample_count++;
+    if (currentSampleTime >= nextSampleTime) {
+        nextSampleTime = currentSampleTime + 20;
+        sample_count++;
 
-    // Read ADC and center so +-512
-    float sample = (float)analogRead(0)-503.f;
+        // Read ADC and center so +-512
+        float sample = (float)analogRead(0)-503.f;
 
-    // Filter only bass component
-    float value = bassFilter(sample);
+        // Filter only bass component
+        float value = bassFilter(sample);
 
-    // Take signal amplitude and filter
-    float envelope = envelopeFilter(abs(value));
+        // Take signal amplitude and filter
+        float envelope = envelopeFilter(abs(value));
 
-    // Every 200 samples (25hz) filter the envelope 
-    if(sample_count >= 200) {
-        // Filter out repeating bass sounds 100 - 180bpm
-        float beat = beatFilter(envelope);
+        // Every 200 samples (25hz) filter the envelope 
+        if(sample_count >= 200) {
+            // Filter out repeating bass sounds 100 - 180bpm
+            float beat = beatFilter(envelope);
 
-        // Threshold it based on potentiometer on AN1
-        float thresh = 40;
+            // Threshold it based on potentiometer on AN1
+            float thresh = 40;
 
-        // If we are above threshold, light up LED
-        if(beat < thresh) analogWrite(5, 0);
-        else analogWrite(5, 255);
+            // If we are above threshold, light up LED
+            //if(beat < thresh) analogWrite(5, 0);
+            //else analogWrite(5, 255);
+            if(beat > thresh) {
+                howBumpingIsIt = ITS_TOTALLY_LIT; // ヽ( •_)ᕗ
+            }
 
-        //Reset sample counter
-        sample_count = 0;
+            //Reset sample counter
+            sample_count = 0;
+        }
     }
+
+    if (howBumpingIsIt) { SoundFadeDelayless(); }
+}
+
+void BandpassFilterBeat::writeAndDelay(unsigned int brightness, unsigned int ms) {
+  const int maxDutyCycle = 255;
+  analogWrite(ledPin, brightness * maxDutyCycle / 255);
+  next_blink = millis() + ms;
+}
+
+void BandpassFilterBeat::SoundFadeDelayless() {
+  if (millis() >= next_blink) {
+    if (MODE == MODE_INSTANT) {
+      switch (howBumpingIsIt) {
+        case  2: writeAndDelay(255, 16); break;
+        case  1: writeAndDelay(  5, 16); break;
+      }
+    } else {
+      switch (howBumpingIsIt) {
+        case 27: writeAndDelay(255, 20/2); break;
+        case 26: writeAndDelay(250, 20/2); break;
+        case 25: writeAndDelay(240, 20/2); break;
+        case 24: writeAndDelay(230, 20/2); break;
+        case 23: writeAndDelay(220, 20/2); break;
+        case 22: writeAndDelay(210, 30/2); break;
+        case 21: writeAndDelay(200, 30/2); break;
+        case 20: writeAndDelay(190, 30/2); break;
+        case 19: writeAndDelay(180, 30/2); break;
+        case 18: writeAndDelay(170, 20/2); break;
+        case 17: writeAndDelay(160, 30/2); break;
+        case 16: writeAndDelay(150, 40/2); break;
+        case 15: writeAndDelay(140, 50/2); break;
+        case 14: writeAndDelay(130, 50/2); break;
+        case 13: writeAndDelay(120, 60/2); break;
+        case 12: writeAndDelay(110, 50/2); break;
+        case 11: writeAndDelay(100, 50/2); break;
+        case 10: writeAndDelay( 90, 50/2); break;
+        case  9: writeAndDelay( 80, 50/2); break;
+        case  8: writeAndDelay( 70, 30/2); break;
+        case  7: writeAndDelay( 60, 30/2); break;
+        case  6: writeAndDelay( 50, 30/2); break;
+        case  5: writeAndDelay( 40, 30/2); break;
+        case  4: writeAndDelay( 30, 30/2); break;
+        case  3: writeAndDelay( 20, 30/2); break;
+        case  2: writeAndDelay( 10, 30/2); break;
+        case  1: writeAndDelay(  5, 10/2); break;
+      }
+    }
+    howBumpingIsIt--;
+  }
 }
--- Firmware/combined/bandpass_filter_beat.hpp
+++ Firmware/combined/bandpass_filter_beat.hpp
@@ -4,11 +4,23 @@ class BandpassFilterBeat: public Mode {
     void loop();
   
   private:
-  	//defines for setting and clearing register bits
-  	static const void cbi(char sfr, char bit) { _SFR_BYTE(sfr) &= ~_BV(bit); }
-  	static const void sbi(char sfr, char bit) { _SFR_BYTE(sfr) |= _BV(bit); }
+    static const int MODE_INSTANT = 0;
+    static const int MODE_FADE = 1;
+    static const int MODE;
+    unsigned long next_blink = 0;
+
+    //defines for setting and clearing register bits
+    static const void cbi(char sfr, char bit) { _SFR_BYTE(sfr) &= ~_BV(bit); }
+    static const void sbi(char sfr, char bit) { _SFR_BYTE(sfr) |= _BV(bit); }
     
     const float bassFilter(float sample);
     const float envelopeFilter(float sample);
     const float beatFilter(float sample);
+
+    //light fade
+    unsigned int howBumpingIsIt = 0;
+    const unsigned int ITS_TOTALLY_LIT = MODE == MODE_INSTANT ? 2 : 27;
+
+    void writeAndDelay(unsigned int brightness, unsigned int ms);
+    void SoundFadeDelayless();
 };
